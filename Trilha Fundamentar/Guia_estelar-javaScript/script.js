f91ce27a511a6d52b5045dfc608f2200308b90e0
Hederson_Sousa <80648653+Sousa-heder@users.noreply.github.com>
2022-10-31 18:02:22 -0300

    JS
    Algumas informações sobre tipo de dados, e scope em js

---
+++ Trilha Fundamentar/Guia_estelar-javaScript/script.js
@@ -0,0 +1,24 @@
+var clima = "Quente"
+let temp = "12:12"
+const pos = 10
+
+console.log(clima, temp, pos)
+
+console.log('>Existe x antes do bloco', x)
+
+{
+    var x = 0
+}
+
+console.log('>Existe x antes do bloco', x)
+
+{   
+    let y = 0
+    console.log('>Existe y no bloco', y)
+}
+
+
+
+
+
+
