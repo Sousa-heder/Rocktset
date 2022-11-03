59d54515fc4a322b9dd76002b146af61fc5cdc37
Hederson_Sousa <80648653+Sousa-heder@users.noreply.github.com>
2022-11-03 17:26:29 -0300

    Conclusão da aula sobre funcões
    Fim da parte sobre funções. Iniciando a parte sobre manipulação de dados.

--- Trilha Fundamentar/Guia_estelar-javaScript/functions.js
+++ Trilha Fundamentar/Guia_estelar-javaScript/functions.js
@@ -13,4 +13,95 @@ creatPhrases()
 
 // end
 console.log("Fim do programa.")
-   
+
+// function expression
+// function anonymous
+let total = 0
+
+// parâmetros (paremeters)
+const sum = function(number1, number2){
+    let total = number1 + number2
+    return total
+}
+
+let number1 = 34
+let number2 = 54
+
+//console.log(`o número 1 é ${number1}`)
+//console.log(`o número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
+console.log(total)
+
+// Função é um liquidificador 
+
+function fazerSuco(fruta1, fruta2){
+    return 'suco de: ' + fruta1 + fruta2
+}
+
+const copo = fazerSuco('Banana com ','maçã')
+
+console.log(copo)
+
+// function scope 
+let subject 
+
+function createThink(){
+    subject = 'study'
+}
+
+console.log(subject)
+createThink()
+console.log(subject)
+
+// function hoisting
+
+sayMyName()
+
+function sayMyName(){
+    console.log('Hed')
+}
+
+// arrow function 
+
+const sayMyName2 = (nome) =>{
+    console.log(nome)
+}
+
+sayMyName2('Hed')
+
+// callback function
+
+function meunome(nomE){
+    console.log('antes de executar a funcao callback')
+
+    nomE()
+
+    console.log('Depois de excutar a callback')
+}
+
+meunome(
+    () => {
+        console.log('estou em uma callback')
+    }
+)
+
+/*
+    Function() constructor
+
+    * expressão new
+    * criar um novo objeto
+    * this keyword
+
+*/
+
+function Person(NOme){
+    this.NOme = NOme
+    this.walk = function(){
+        return this.NOme + "está andando"
+    }
+}
+
+const mayk = new Person("Mayk")
+const Joao= new Person("Joao")
+console.log(mayk.walk())
+console.log(Joao.walk())
